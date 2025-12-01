96ca21cf12a3e9f9d88f587bc721f9cda9173567
Nell Mills <nell@mad-cat.co.uk>
2025-12-01 13:20:01 +0000

    Added max line length for parsing files.

--- Config.h
+++ Config.h
@@ -13,6 +13,10 @@
 #define NM_MAX_ERROR_LENGTH 1024
 #endif
 
+#ifndef NM_MAX_FILE_LINE_LENGTH
+#define NM_MAX_FILE_LINE_LENGTH 512
+#endif
+
 #ifndef NM_MAX_LOD_LEVELS
 #define NM_MAX_LOD_LEVELS 1
 #endif
